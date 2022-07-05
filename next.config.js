c5481b78bd5d3e0bda67f9d47f45ec7eea38524c
Leon Bubova <bubova.leon@gmx.de>
2022-07-05 02:32:50 +0200

    fix assets and basepath for repo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "./",
+  basePath: "/two-pizzas",
+  assetPrefix: "/two-pizzas",
   experimental: {
     images: {
       unoptimized: true,
