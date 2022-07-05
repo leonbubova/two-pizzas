3c22995851ade3e3131fcffdf3580d858375e698
Leon Bubova <bubova.leon@gmx.de>
2022-07-05 02:23:28 +0200

    add out to repo

---
+++ _next/static/N3YcLL5gv7gMe89xOhUYy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2a61f18419001d10.css","static/chunks/pages/index-95634be7a221946f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
