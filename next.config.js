3b7ec5cabfb5cfb5734c85c83b54e020935fe966
Leon Bubova <bubova.leon@gmx.de>
2022-07-05 03:07:32 +0200

    Deploy next to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // basePath: "/two-pizzas",
-  // assetPrefix: "",
+  basePath: "/two-pizzas",
+  assetPrefix: "/two-pizzas",
   experimental: {
     images: {
       unoptimized: true,
