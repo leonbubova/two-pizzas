c906029e8348d64bd1bb50cf92e00d38af94ffb9
Leon Bubova <bubova.leon@gmail.com>
2023-11-18 18:05:51 +0100

    update gitignore

--- out/_next/static/gufFcLYCmJGKXak3MsThi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2a61f18419001d10.css","static/chunks/pages/index-b2f6b3ebbaab2948.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
